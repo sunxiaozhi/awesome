232af3fba553b89fb11c96a79b8570961585a1e4
sunxiaozhi <1129535445@qq.com>
2018-04-16 21:32:09 +0800

    更新redis测试

--- redis/redis/index.php
+++ redis/redis/index.php
@@ -1,31 +1,54 @@
 <?php
-
+/**
+ * redis支持五种数据类型，list、hash、string、set、zset
+ */
 include "RedisOption.php";
 
+/**
+ * 打印函数
+ * @param $var
+ */
+function p($var)
+{
+    if (is_bool($var)) {
+        var_dump($var);
+    } else if (is_null($var)) {
+        var_dump(NULL);
+    } else {
+        echo "<pre style='position:relative;z-index:1000;padding:10px;border-radius:5px;background:#F5F5F5;border:1px solid #aaa;font-size:14px;line-height:18px;opacity:0.9;'>" . print_r($var, true) . "</pre>";
+    }
+}
+
+//配置信息
 $config = array(
-    'host' => '192.168.20.55',
+    'host' => '192.168.0.51',
     'port' => '6379',
-    'auth' => '123',
+    'auth' => '123456',
 );
 
 $attr = array(
-    'db_id' => 6,
+    'db_id' => 0,
 );
 
-$redis1 = RedisOption::getInstance($config, $attr);
+$redis_one = RedisOption::getInstance($config, $attr);
 
-$d = $redis1->set( "testKey2" , "Hello Redis"); //设置测试key
+/*测试string类型*/
+$redis_one->set( "testKey" , "Hello Redis"); //设置测试key
+p($redis_one->get("testKey"));//输出value
 
-var_dump($d);
+/*测试list类型*/
 
-echo $redis1->get("testKey") . '<br>';//输出value
+/*测试hash类型*/
+$redis_one->hSet('hash', 'key1', 'hello1');
+$redis_one->hSet('hash', 'key2', 'hello2');
+p($redis_one->hGet('hash', 'key1'));
+p($redis_one->hGet('hash', 'key2'));
+/*测试set类型*/
 
-$redis2 = RedisOption::getInstance($config, $attr);
+/*测试zset类型*/
 
-$redis2->set( "testKey4" , "Hello Redis"); //设置测试key
-
-echo $redis2->get("testKey") . '<br>';//输出value
-
-$redis2->hSet('h', 'key1', 'hello');
-$redis2->hSet('h', 'key2', 'hello2');
-echo $redis2->hGet('h', 'key1');
+$redis_two = RedisOption::getInstance($config, $attr);
+//$redis_two->flushDB();
+p($redis_two->ttl('hash'));
+p($redis_two->keys('*'));
+p($redis_two->dbSize());
